f3ba8517c3e276a1ee59472e4a796fa84e041e4d
Neal Sutaria <nealsutaria99@gmail.com>
2018-12-30 04:35:29 -0600

    javascript changes and chnaged to menu bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,11 +17,18 @@ $(document).ready(function () {
 
  });
 
+$(document).ready(function () {
+      $('#expens_description').on('change',function(){
+       var selectedValue = $(this).val();
+        selectedValue == "Other" ? $("#otherDes").show() : $("#otherDes").hide();
+      });
 
+
+ });
 
 
 // $(document).ready(function () {
-//   if ($('#expense_description').val() == "Other") {
+//   if ($('#expens_description').val() == "Other") {
 //     $("#otherDesc").show();
 
 //   } else {
