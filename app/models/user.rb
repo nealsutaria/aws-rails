a6984f31a37b2bda0a7f93bb77f21877237b8c42
Neal Sutaria <nealsutaria99@gmail.com>
2019-02-13 20:39:24 -0600

    password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :records, dependent: :destroy
   # validates_strength_of :password, :with => :email, :level => :good
-  validates :password, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/, message: "must include at least one lowercase letter, one uppercase letter, one digit, and one special character" }
+  # validates :password, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/, message: "must include at least one lowercase letter, one uppercase letter, one digit, and one special character" }
+PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+/x
 
+validates :password,
+  presence: true,
+  length: { in: Devise.password_length, message: "must include at least one lowercase letter, one uppercase letter, one digit, one special character " },
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  # message: "must include at least one lowercase letter, one uppercase letter, one digit, and one special character",
+  on: :create
+
+validates :password,
+  allow_nil: true,
+  length: { in: Devise.password_length, message: "must include at least one lowercase letter, one uppercase letter, one digit,one special character"},
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  on: :update
 end
