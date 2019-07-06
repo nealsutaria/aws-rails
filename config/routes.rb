e5f608256d3ad1f71e59930a07e84d8a506fba80
Neal Sutaria <nealsutaria99@gmail.com>
2019-07-06 18:26:30 -0500

    Commented out business route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "search", to: "records#search", as: "search"
-  get "business", to: "records#business", as: "business"
+  # get "business", to: "records#business", as: "business"
 
   get "terms", to: "records#terms", as: "terms"
   get "privacy", to: "records#privacy", as: "privacy"
