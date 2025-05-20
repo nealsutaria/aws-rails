1a485ac3daba9419c281c649a6480ed7d4927eb3
neal-sutaria <97972270+neal-sutaria@users.noreply.github.com>
2025-05-20 13:47:21 -0500

    test1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 #FIXME replace 1.2.3.4 with your IP addressss
-server '98.84.163.2', user: 'deploy', roles: %w{web app db}
+server '44.206.224.108', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
 
